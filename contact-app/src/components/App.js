45dec85b9a20a848c4e26e97c34a4443b0ec7b99
WilsonFemi <oniseikeji@gmail.com>
2022-04-14 22:53:26 -0400

    check point 2

---
+++ contact-app/src/components/App.js
@@ -0,0 +1,13 @@
+import React from "react"
+import './App.css';
+import Header from "./Header"
+import AddContact from "  "
+
+
+function App() {
+  return (
+    <div>Hello World from app.js </div>
+  );
+}
+
+export default App;
